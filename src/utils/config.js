b8af50d3dc49628734bac16cef430cedaf51cd2d
Guanzhao Mo <502442134@qq.com>
2016-10-31 23:10:09 +0800

    增加轮播指令

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,16 @@
 let nprogress = require("./nprogress.js");
 let fastclick = require("./fastclick.js");
 let requireManage = require("./require.js");
+let lazyLoad = require("./vue-lazyload.js");
+
+
 window.requireManage = requireManage;
 
 new fastclick(document.body);
+
+Vue.use(lazyLoad, {
+  try: 3 // default 1
+});
 
 
 module.exports = {}
