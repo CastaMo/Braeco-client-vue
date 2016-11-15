21be99f300f2d5968f3b30e3233fd804df04bdca
Guanzhao Mo <502442134@qq.com>
2016-11-16 01:38:48 +0800

    完成food-property的大体结构样式70%

---
+++ src/pages/Menu/components/food-property.vue
@@ -0,0 +1,63 @@
+<template>
+    <transition name="fade" mode="out-in">
+        <div id='food-property' class='modal' v-show="showFlag">
+            <div class='cover' v-on:click="showFlag = !showFlag"></div>
+            <transition name='modal' mode="out-in">
+                <div class='modal-dialog adapt-center' v-show="showFlag">
+                    <div class='modal-content'>
+                        <div class='modal-header'>
+                            <b class='icon-close'></b>
+                            <p>杯口特调</p>
+                        </div>
+                        <div class='modal-body'>
+                            <div class='property'>
+                                <div class='property-wrapper'>
+                                    <div class='property-container'></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='modal-footer'>
+                            <button class='braeco-btn yellow confirm-btn'>加入购物车</button>
+                        </div>
+                    </div>
+                </div>
+            </transition>
+        </div>
+    </transition>
+</template>
+
+
+<script>
+
+module.exports = {
+    name: 'food-property',
+    data() {
+        return {
+            showFlag: false
+        }
+    },
+    created() {
+        let vm = this;
+        this.$root.$on("root:food-property", function(food) {
+            console.log(food);
+            vm.showFlag = true;
+        });
+    },
+    watch: {
+        showFlag(newFlag, oldFlag) {
+            this.$root.$emit("root:trolley-footer-bar-show", !newFlag);
+        }
+    }
+}
+
+</script>
+
+<style scoped lang="less">
+
+.property {
+    .property-wrapper {
+        height: 220px;
+    }
+}
+
+</style>
