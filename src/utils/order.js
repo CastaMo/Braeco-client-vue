88f185acb091d4477491f70f27a20c54d0b1b5ab
Guanzhao Mo <502442134@qq.com>
2016-11-23 17:29:04 +0800

    将对orderForTrolley的操作逻辑从主mian.js迁移到utils.order中

--- src/utils/order.js
+++ src/utils/order.js
@@ -14,7 +14,46 @@ const order = {
 		}
 		temp.num = num;
 		return temp;
-	}
+	},
+    tryGetOrderItemByFoodId(orderForTrolley, foodId, isExtend) {
+        let temp = null;
+        orderForTrolley.forEach(function(orderItem) {
+            if (Number(foodId) === Number(orderItem.id)) {
+                temp = orderItem;
+                return false;
+            }
+        });
+
+        // 若找不到则新建一个
+        if (!temp && isExtend) {
+            orderForTrolley.push(temp = {
+                id: foodId,
+                subItems: []
+            });
+        }
+        return temp;
+    },
+    tryGetSubItemByGroups(subItems, groups, isExtend) {
+        let temp = null;
+        subItems.forEach(function(subItem) {
+            // 若为普通单品或者groups项相同，则匹配成功
+            if (
+                !groups
+            ||  JSON.stringify(subItem.groups) === JSON.stringify(groups)
+            ) {
+                temp = subItem;
+                return null;
+            }
+        });
+        // 若找不到则新建一个
+        if (!temp && isExtend) {
+            orderItem.subItems.push(temp = {
+                num: 0,
+                groups: groups
+            });
+        }
+        return temp;
+    }
 }
 
 module.exports = order;
