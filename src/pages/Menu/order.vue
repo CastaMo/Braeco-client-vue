4d3851004c271b1fa92f2b518147f24c24b480d2
Guanzhao Mo <502442134@qq.com>
2016-11-30 00:48:59 +0800

    将优惠信息应用到order.vue中

--- src/pages/Menu/order.vue
+++ src/pages/Menu/order.vue
@@ -33,6 +33,32 @@
                         </li>
                     </ul>
                 </div>
+                <div class='discount-container'>
+                    <div class='discount-title'>
+                        <div class='margin-left-wrapper discount-title-wrapper'>
+                            <div class='discount-title-container'>
+                                优惠折扣
+                            </div>
+                        </div>
+                    </div>
+                    <ul
+                        class='discount-item-list'
+                    >
+                        <li
+                            class='discount-item'
+                            v-for="(value, type) of discountMap"
+                            v-if="value"
+                        >
+                            <div class='discount-item-wrapper margin-left-wrapper'>
+                                <div class='discount-item-container'>
+                                    <div class='discount-type left-part'>{{discountNameMap[type]}}</div>
+                                    <div class='discount-value right-part'>{{Number(value.toFixed(2))}}</div>
+                                    <div class='clear'></div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -45,7 +71,16 @@ module.exports = {
         return {
             orderItems: [],
             groupsMap: {},
-            dishLimit: null
+            dishLimit: null,
+            discountMap: {},
+            discountNameMap: {
+                "half": "第二份半价",
+                "discount": "折扣优惠",
+                "sale": "立减优惠",
+                "reduce": "满减优惠",
+                "userDiscount": "会员优惠",
+                "coupon": "代金券"
+            }
         }
     },
     created() {
@@ -60,6 +95,7 @@ module.exports = {
         this.$root.$watch('tempData.orderForTrolley', function(newData) {
             vm.orderItems = vm.getItemsForOrder();
         }, {deep: true});
+        this.discountMap = this.$root.tempData.discountMap;
     },
     components: {
         'order-item': Vue.component('order-item')
@@ -137,6 +173,36 @@ module.exports = {
             line-height: 44px;
             .title {
                 font-weight: bold;
+            }
+            &:last-of-type {
+                .margin-left-wrapper {
+                    border-bottom: none;
+                }
+            }
+        }
+    }
+}
+
+.discount-container {
+    margin-bottom: 16px;
+    border-top: solid 1px #C8C7CC;
+    border-bottom: solid 1px #C8C7CC;
+    background-color: #fff;
+    .discount-title {
+        line-height: 44px;
+        font-weight: bold;
+    }
+    ul.discount-item-list {
+        li.discount-item {
+            line-height: 44px;
+            .discount-item-container {
+                padding-right: 16px;
+                .discount-value {
+                    &:before {
+                        content: '- ￥';
+                        font-size: 14px;
+                    }
+                }
             }
             &:last-of-type {
                 .margin-left-wrapper {
