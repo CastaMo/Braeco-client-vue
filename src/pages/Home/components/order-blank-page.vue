e386e564c2ce5e5095120bd745f1e30d0d809622
Guanzhao Mo <502442134@qq.com>
2016-12-02 00:45:04 +0800

    初始化menu-order页面，添加订单为空时的样式展示以及事件逻辑

---
+++ src/pages/Home/components/order-blank-page.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id='order-blank-page'>
+        <div class='order-blank-wrapper'>
+            <div class='order-blank-container'>
+                <div class='menu-icon-field'>
+                    <div class='menu-icon' v-on:click="iconClickEvent"></div>
+                </div>
+                <div class='word-field'>
+                    <div class='word'>这里没有订单</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+module.exports = {
+    name: 'order-blank-page',
+    methods: {
+        iconClickEvent() {
+            this.$emit("menu-icon-click");
+        }
+    }
+}
+
+</script>
+
+<style scoped lang="less">
+
+.background-norm(@elem: "none") {
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+#order-blank-page {
+    .order-blank-wrapper {
+        .order-blank-container {
+            padding-top: 100px;
+        }
+    }
+}
+
+.menu-icon-field {
+    .menu-icon {
+        width: 180px;
+        height: 180px;
+        .background-norm();
+        background-size: 180px 180px;
+        background-image: url("../../../assets/menu-icon.png");
+        margin: 0 auto;
+    }
+}
+
+.word-field {
+    margin-top: 56px;
+    text-align: center;
+}
+
+</style>
