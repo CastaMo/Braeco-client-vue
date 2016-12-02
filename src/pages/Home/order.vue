0216675382d670d5b65afa3aaf3e87d0b9fe5521
Guanzhao Mo <502442134@qq.com>
2016-12-02 12:18:06 +0800

    为home-order增加异步处理

--- src/pages/Home/order.vue
+++ src/pages/Home/order.vue
@@ -17,10 +17,25 @@ module.exports = {
     },
 
 	created() {
-
+        if (this.$root.isLoaded) {
+            this.init();
+        }
+        let vm = this;
+        this.$root.$on("root:getData", function() {
+            setTimeout(function() {
+                vm.init();
+            }, 200);
+        });
 	},
 
+    beforeDestroy() {
+        this.$root.$off("root:getData");
+    },
+
     methods: {
+        init() {
+
+        },
         routeToMenu() {
             let categoryId = this.$root.requireData.menu.categories[0].id;
             this.$root.$router.push(`/menu/main/${categoryId}`);
