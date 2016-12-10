3ab30a220f8cc0d71de8f571068ff9bc62b35adb
Guanzhao Mo <502442134@qq.com>
2016-12-11 01:30:24 +0800

    注释fakeData

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,19 +2,19 @@ const mutations = {
     getData(state, payload) {
         state.requireData = payload.requireData;
         state.isLoaded = true;
-        state.requireData.member_info = {
-            EXP: 17837,
-            address: {
-                address: "广东省广州市番禺区 中二横路/中四路(路口)",
-                detail: "哈哈哈"
-            },
-            avatar: "http://static.brae.co/images/avatar/default.png",
-            balance: 9672.96,
-            mobile: "18819473259",
-            nickname: "冠钊",
-            sex: 0,
-            user: 2
-        }
+        // state.requireData.member_info = {
+        //     EXP: 17837,
+        //     address: {
+        //         address: "广东省广州市番禺区 中二横路/中四路(路口)",
+        //         detail: "哈哈哈"
+        //     },
+        //     avatar: "http://static.brae.co/images/avatar/default.png",
+        //     balance: 9672.96,
+        //     mobile: "18819473259",
+        //     nickname: "冠钊",
+        //     sex: 0,
+        //     user: 2
+        // }
     }
 };
 
